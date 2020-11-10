7ba5d04d3f970190d856e07498c9630a255532d6
chandrunarayan <54154611+chandrunarayan@users.noreply.github.com>
2020-11-09 19:09:38 -0800

    initial

---
+++ Doodad.java
@@ -0,0 +1,10 @@
+class Doodad
+{
+  private double myNum;
+  public Doodad()
+  {
+    myNum = 0;
+  }
+  public double getNum(){/* your java code */}
+  public void setNum(double n){/* your java code */}
+}
